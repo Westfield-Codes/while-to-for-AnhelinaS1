0ea20737a1e1961a20d707b75455f453cfb8d96d
AnhelinaS1 <anhelina207742@student-schoolsofwestfield.org>
2025-04-04 14:27:56 +0000

    done multiplication while loop

--- script.js
+++ script.js
@@ -1,20 +1,12 @@
 /* Multiplication */
-
-// Create 2 variables, a & b, and set to ne random integers between 3 and 9
-var a = Math.floor(math.random()*10-3)+3;
-var b = Math.floor(math.random()*10-3)+3;
-// Set a variable called product to be the product of a and b
+var question = 1;
+while (question<=5){
+    question++;
+var a = Math.floor(Math.random()*(10-3))+3;
+var b = Math.floor(Math.random()*10-3)+3;
 var product = a*b;
-// Set a variable called equation to display  a * b = ??
-var equation = alert(a + "*" + b + "=??");
-// Ask the equation of the user and store the user input in a variable called answer
+var equation = a + "*" + b + "=?";
 answer = prompt(equation);
-// Say "Correct!" if the answer and product match, "Incorrect!" if not.
 if (answer == product) alert ("Correct!");
 else alert ("Incorrect!");
-/* Multiplication whjile Loop */
-
-// Embed all this code in a While loop and run it 5 times.
-
-/* Multiplication For Loop */
-// Convert the while loop to a for loop. 
+}
